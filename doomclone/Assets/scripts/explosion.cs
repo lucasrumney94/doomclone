aa1d911497a357d3b2fdfb39a46a898388e2e4dd
Lucas Rumney <lucasrumney94@gmail.com>
2015-06-04 16:31:12 -0400

    Fixed Explosion Code to Detect Colliders within explosion Radius

--- doomclone/Assets/scripts/explosion.cs
+++ doomclone/Assets/scripts/explosion.cs
@@ -4,12 +4,22 @@ using System.Collections;
 public class explosion : MonoBehaviour {
 
 	public float explosiveDamage;
+	public float radius = 2.0f;
 	private int i = 0;
+
+	private bool switchForStay = false;//this is the boolean flag so that the explosion enters  
 
 	// Use this for initialization
 	void Start () 
 	{
-
+		Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+		int j = 0;
+		while (j < hitColliders.Length) 
+		{
+			hitColliders[j].SendMessageUpwards("applyDamage",explosiveDamage,SendMessageOptions.DontRequireReceiver);
+			Debug.Log (hitColliders[j].name);
+			j++;
+		}
 	}
 
 	void Update()
@@ -23,7 +33,11 @@ public class explosion : MonoBehaviour {
 
 	void OnCollisionEnter(Collision c)
 	{
-		c.collider.SendMessageUpwards("applyDamage",explosiveDamage, SendMessageOptions.DontRequireReceiver);
+//		if (switchForStay)
+//		{
+//			c.collider.SendMessageUpwards("applyDamage",explosiveDamage, SendMessageOptions.DontRequireReceiver);
+//			switchForStay = false;
+//			Physics.OverlapSphere
+//		}
 	}
-
 }
