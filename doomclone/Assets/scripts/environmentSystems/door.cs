efa9e5aeb904b8e682ab532b3454312974ef1bc9
Lucas Rumney <lucasrumney94@gmail.com>
2015-06-05 18:05:17 -0400

    New Art Version with basic room lighting

---
+++ doomclone/Assets/scripts/environmentSystems/door.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class door : MonoBehaviour {
+
+	public float doorHeight; //How far the door will move
+	public float doorSpeed;
+	public float doorCloseRange;
+
+
+	private Transform player;
+	private Vector3 originalPosition;
+	private bool openMe;
+	private bool closeMe;
+
+
+
+	void Start()
+	{
+		originalPosition = GetComponentInParent<Transform>().position;
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		StartCoroutine("closeDetect");
+
+	}
+
+	void Update()
+	{
+		if (openMe && transform.position.y - originalPosition.y < doorHeight) {
+			transform.position += new Vector3 (0, doorSpeed, 0);
+		} 
+		else 
+		{
+			openMe = false;
+		}
+		if (!openMe && closeMe && transform.position.y - originalPosition.y > 0) {
+			transform.position -= new Vector3 (0, doorSpeed, 0);
+		} 
+		else 
+		{
+			closeMe = false;
+		}
+		//transform.position = new Vector3 (Mathf.PingPong (Time.time, doorWidth), transform.position.y, transform.position.z);
+	}
+
+
+
+	void interact()
+	{
+		openMe = true;
+	}
+
+	IEnumerator closeDetect()
+	{
+		for (;;) //always
+		{ 
+			if (Vector3.Distance(transform.position,player.position)> doorCloseRange)
+			{	
+				closeMe = true;
+			}
+			//Debug.Log(closeMe);
+			yield return new WaitForSeconds(.2f);
+		}
+	}
+
+}
