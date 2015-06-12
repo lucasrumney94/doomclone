c59fb57ab362477949a6c322c43b8cf208fbd8f6
Lucas Rumney <lucasrumney94@gmail.com>
2015-06-12 11:54:15 -0400

    Implemented basic Music trigger Prefab and Music code.

---
+++ doomclone/Assets/scripts/environmentSystems/musicTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class musicTrigger : MonoBehaviour {
+
+	public int changeSongTo;
+
+	private bool tripped = false;
+
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.tag == "Player" && !tripped)
+		{
+			GameObject.FindGameObjectWithTag("MusicController").GetComponent<musicController>().changeTo(changeSongTo);
+			tripped = true;
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ doomclone/Assets/scripts/sound Controllers/musicController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class musicController : MonoBehaviour {
+	
+	public AudioClip[] music;
+
+	public int defaultSong = 0; 
+
+
+
+
+	private int currentSong;
+	private AudioSource musicSource;
+
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		musicSource = this.gameObject.GetComponent<AudioSource>();
+
+		musicSource.clip = music[defaultSong];
+
+		musicSource.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (!musicSource.isPlaying)
+		{
+			changeTo(0);
+		}
+
+
+	}
+
+	public void changeTo(int song)
+	{	
+		musicSource.clip = music[song];
+
+		musicSource.Play();
+	}
+
+
+}
